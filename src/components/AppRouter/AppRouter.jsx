6f6df7f3004c4908b018d3f942ca91f49e47666c
anastasya00 <anastasyadancko@yandex.ru>
2024-11-01 13:44:55 +0700

    Using constants in the AppRouter from consts

--- src/components/AppRouter/AppRouter.jsx
+++ src/components/AppRouter/AppRouter.jsx
@@ -9,6 +9,7 @@ import { HomePage } from "../../pages/home-page";
 import { CartPage } from "../../pages/cart-page";
 import { store } from "../../redux";
 import { Header } from "../../components/header";
+import { SHOP_ROUTE, BASKET_ROUTE } from "../consts";
 import './AppRouter.css';
 
 export const AppRouter = () => {
@@ -18,8 +19,8 @@ export const AppRouter = () => {
         <div className="AppRouter">
           <Header></Header>
           <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/cart" element={<CartPage />} />
+            <Route path={SHOP_ROUTE} element={<HomePage />} />
+            <Route path={BASKET_ROUTE} element={<CartPage />} />
           </Routes>
         </div>
       </Router>
